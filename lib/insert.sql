d67e725904600a4599ae506e27838e582c38870f
ryangachahic <ryangachahi230@gmail.com>
2025-09-02 09:07:08 +0300

    intial commit

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+  (1, 'Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+  (2, 'Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+  (3, 'Tabitha', 8, 'F', 'Cinnamon', 'Friendly', 1),
+  (4, 'Sergeant Brown', 12, 'M', 'Dark Brown', 'Serious', 1),
+  (5, 'Melissa', 7, 'F', 'White', 'Gentle', 1),
+  (6, 'Grinch', 15, 'M', 'Gray', 'Grumpy', 0),
+  (7, 'Wendy', 4, 'F', 'Golden', 'Playful', 1),
+  (8, NULL, 3, 'M', 'Black', 'Unknown', 1);
